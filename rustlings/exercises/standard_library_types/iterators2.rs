a59a9618c018355856e76602349f579c93b39fc3
Official-Echo <o.zolotarov@ukma.edu.ua>
2024-10-11 01:08:06 +0300

    Completed standard functions

--- rustlings/exercises/standard_library_types/iterators2.rs
+++ rustlings/exercises/standard_library_types/iterators2.rs
@@ -2,9 +2,6 @@
 // In this exercise, you'll learn some of the unique advantages that iterators
 // can offer. Follow the steps to complete the exercise.
 // As always, there are hints if you execute `rustlings hint iterators2`!
-
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -12,7 +9,11 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            let mut result = first.to_uppercase().collect::<String>();
+            result.extend(c);
+            result
+        }
     }
 }
 
@@ -21,7 +22,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
 // Step 3.
@@ -29,7 +30,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    words
+        .iter()
+        .map(|&word| capitalize_first(word))
+        .collect::<Vec<String>>()
+        .join("")
 }
 
 #[cfg(test)]
